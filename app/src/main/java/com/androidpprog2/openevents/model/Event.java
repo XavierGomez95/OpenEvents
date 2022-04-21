9e22f5dcae887f13982caaf89af4e5227f7199f8
Xavier Gómez de la Torre <xaviergomezxcv@gmail.com>
2022-04-21 17:57:57 +0200

    21/04 XGT: Model Classes

---
+++ app/src/main/java/com/androidpprog2/openevents/model/Event.java
@@ -0,0 +1,17 @@
+package com.androidpprog2.openevents.model;
+
+import java.util.Date;
+
+public class Event {
+    int id;
+    String name;
+    int ownerId;
+    String date;
+    String image;
+    String location;
+    String description;
+    Date eventStart;
+    Date eventEnd;
+
+
+}
---
+++ app/src/main/java/com/androidpprog2/openevents/model/User.java
@@ -0,0 +1,16 @@
+package com.androidpprog2.openevents.model;
+
+import java.util.List;
+
+public class User {
+    int id;
+    String name;
+    String lastName;
+    String email;
+    String password;
+    String image;
+    List<String> friends;
+    List<String> events;
+
+
+}
