c31fc463a6e743d650bb3814ee5d716d9610c1c5
Iris <irisquerol1998@gmail.com>
2022-04-30 08:32:54 +0200

    30/04 IQA: Api User functions + register user call

--- app/src/main/java/com/androidpprog2/openevents/business/User.java
+++ app/src/main/java/com/androidpprog2/openevents/business/User.java
@@ -12,5 +12,11 @@ public class User {
     List<String> friends;
     List<String> events;
 
-
+    public User(String name, String lastName, String email, String password, String image) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.image = image;
+    }
 }
--- app/src/main/java/com/androidpprog2/openevents/view/activities/RegisterActivity.java
+++ app/src/main/java/com/androidpprog2/openevents/view/activities/RegisterActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import com.androidpprog2.openevents.R;
+import com.androidpprog2.openevents.api.APIUser;
+import com.androidpprog2.openevents.business.User;
+
 
 public class RegisterActivity extends AppCompatActivity {
     private Button registerButton;
+    private EditText name;
+    private EditText lastName;
+    private EditText email;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +32,31 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton = findViewById(R.id.btn_register);
 
         registerButton.setOnClickListener(view -> {
-            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            checkData();
         });
     }
+
+    private void checkData() {
+        name = findViewById(R.id.r_name);
+        lastName = findViewById(R.id.r_last_name);
+        email = findViewById(R.id.r_email);
+        pass = findViewById(R.id.r_password);
+        EditText passAgain = findViewById(R.id.r_password_again);
+        if (pass.getText().toString().equals(passAgain.getText().toString())) {
+            User u = new User(name.getText().toString(), lastName.getText().toString(), email.getText().toString(), pass.getText().toString(), "Commons_Quality_images.htm");
+            APIUser api = APIUser.getInstance();
+            api.addUser(u, new Callback<User>() {
+                @Override
+                public void onResponse(Call<User> call, Response<User> response) {
+                    startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                }
+
+                @Override
+                public void onFailure(Call<User> call, Throwable t) {
+
+                }
+            });
+        }
+    }
 }
+
